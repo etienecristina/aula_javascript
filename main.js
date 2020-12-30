6c6487ebe5c52a11fb1e7b125be7da8ce8c59916
etienecristina <etiene.scristina@gmail.com>
2020-12-30 10:28:42 -0300

    Commit iniciais do projeto aula

---
+++ main.js
@@ -0,0 +1,100 @@
+/*function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+    //console.log(document.getElementById("agradecimento"));
+
+function redirecionar(){
+    window.open("https://www.google.com/");
+    }
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse" 
+}
+
+function voltar(elemento){
+    elemento.innerHTML= "Obrigado por passar o mouse";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+
+}*/
+/*function soma(n1, n2){
+    return n1+n2;
+}*/
+
+/*var data = new Date();
+alert(data.getMinutes());
+alert(data.getHours());
+alert(data.getDay());
+alert(data.getMonth()+1);
+*/
+
+/*var count;
+for (count=0; count <=5; count++){
+    alert(count);
+};*/
+
+/*var count = 0;
+while (count <=5){
+    //console.log(count);
+    //count = count +1
+    alert(count);
+    count++;
+}*/
+
+/*var idade = prompt("Qual sua idade?")
+if (idade >= 18) {
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade");
+}*/
+
+/*var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+/*var validar = 0
+
+function validaIdade(idade){
+    validar;   
+    if (idade >= 18){
+        validar = true
+    }
+    else{
+        validar = false
+    }
+    return validar;
+
+}
+
+var idade = prompt("Qual sua idade?")
+validaIdade(idade)
+console.log(validar);*/
+
+//alert(soma(5, 10));
+
+/*var fruta = {nome:"maça", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+//var lista = ["maça","pera","abacate"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Etiene";
+//alert("Bem vindo " + nome);
+//var idade = 25;
+//alert(nome + " tem " + idade + " anos.")
+//var frase= "O Japão é o melhor time do mundo";
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão" , "Brasil"))
